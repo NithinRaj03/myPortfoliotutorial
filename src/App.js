4f5137213f1af774045a63316b0db50749d7f0e2
NithinRaj03 <nithinraj.nov@gmail.com>
2021-05-21 02:22:21 +0530

    about section added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import Fade from "react-reveal";
+import { Parallax } from "react-parallax";
+import { Container } from "react-bootstrap";
 
 //components
 import MyNavBar from "./components/my-navbar/mynavbar.component";
-
-import './App.css';
+import MyCarousal from "./components/my-carousel/my-carousel.component";
+import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about.component";
 
 function App() {
   return (
+    <div className="App" style={{ position: "relative" }}>
+      <MyNavBar />
+      <MyCarousal />
+      <TitleMessage />
+
       <div>
-        <MyNavBar />
-        My Portfolio
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
       </div>
+    </div>
   );
 }
 
